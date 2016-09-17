42a41b70f2fd4ff85ea6cbbc0b4df6f44dbec5da
HOM\miche_000 <michel@hostettler-edv.ch>
2016-09-17 17:26:14 +0200

    - included watch-counts in gameover scene - implemented counter for watches

---
+++ Focus-HackZurich/Assets/ApplicationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class ApplicationModel
+{
+	static public int tachoCount = 0;
+	static public int rightDoorCount = 0;
+	static public int leftDoorCount = 0;
+	static public int backMirrorCount = 0;
+	static public int phoneCount = 0;
+}
+
--- Focus-HackZurich/Assets/raycastHit.cs
+++ Focus-HackZurich/Assets/raycastHit.cs
@@ -15,6 +15,17 @@ public class raycastHit : MonoBehaviour {
 
 		if (Physics.Raycast (ray, out hit, 100)) {
 			Debug.Log (hit.collider.gameObject.name);
+			if (hit.collider.gameObject.name == "LeftDoor_") {
+				ApplicationModel.leftDoorCount++;
+			} else if (hit.collider.gameObject.name == "RightDoor_") {
+				ApplicationModel.rightDoorCount++;
+			} else if (hit.collider.gameObject.name == "BackMirror_") {
+				ApplicationModel.backMirrorCount++;
+			} else if (hit.collider.gameObject.name == "Tacho_") {
+				ApplicationModel.tachoCount++;
+			} else if (hit.collider.gameObject.name == "SmartPhone_") {
+				ApplicationModel.phoneCount++;
+			}
 		}
 
 		Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
