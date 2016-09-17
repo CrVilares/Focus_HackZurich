b0510b086b89e27dfdc2f703d2b26f6f67469ccf
HOM\miche_000 <michel@hostettler-edv.ch>
2016-09-17 08:15:17 +0200

    Phone call inserted

---
+++ Focus-HackZurich/Assets/PhoneRing.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneRing : MonoBehaviour {
+
+	public AudioSource ring;
+	private Timer timer = new Timer(12.0f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer.UpdateAndTest()){
+			ring.Play ();
+			timer = new Timer (12.0f);
+		}
+	}
+}
